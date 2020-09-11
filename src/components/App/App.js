8ab92b154dadde7de71dcd778a158d107ab70475
Hannah <HannahCheyenne@gmail.com>
2020-09-11 17:41:23 -0500

    client deployed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,7 +14,7 @@ export default class App extends Component {
   state = { hasError: false }
 
   static getDerivedStateFromError(error) {
-    console.error(error)
+    // console.error(error)
     return { hasError: true }
   }
 
