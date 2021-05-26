cc8ad2c68e0f44202234d4d035e5a7a11edaafd3
Hannah <HannahCheyenne@gmail.com>
2021-05-25 20:31:42 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 export default {
-    API_ENDPOINT: `https://capstone1-server.herokuapp.com/api`,
+    PORT: process.env.PORT || 8000,
+    API_ENDPOINT: 
+        process.env.REACT_APP_API_BASE_URL || `http://localhost:8000/api`,
     // API_ENDPOINT: `http://localhost:8000/api`, //for testing locally
     API_KEY: process.env.REACT_APP_API_KEY,
 }
